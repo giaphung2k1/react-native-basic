86f9a84b45f0b22af4dab2addc62d1d7f24b9778
Hung <giaphung2k1@gmail.com>
2025-01-02 10:27:34 +0700

    video 29

--- App.tsx
+++ App.tsx
@@ -1,20 +1,60 @@
-import { StatusBar } from 'expo-status-bar';
+import { useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+
+  // string
+  const [name,setName] = useState<string>('helllo');
+
+  // Number
+  const [age,setAge] = useState<number>(0)
+
+  //
+
+  const [person,setPerson] =  useState<
+    {
+      name: string;
+      age: number;
+    }
+  >({
+      name: 'hung',
+      age: 25
+      
+  });
+
   return (
     <View style={styles.container}>
-      <Text>Hung demo 123</Text>
-      <StatusBar style="auto" />
+      <View>
+        <Text style={styles.text}>{person.name}</Text>
+      </View>
+
+      <Text>{person.age}</Text>
+
     </View>
   );
 }
 
+
+// css in js 
+
 const styles = StyleSheet.create({
+  text: {
+    fontSize:30,
+    color:"red"
+  },
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    // alignItems: 'center',
+    // justifyContent: 'center',
+
+    fontSize:50,
+    color:"red",
+    paddingTop:40,
+    paddingHorizontal: 20
   },
 });
+
+
+
+
