45397dd7245f9df63555c28d8be787b6f4df6726
Hung <giaphung2k1@gmail.com>
2025-01-02 16:33:43 +0700

    video 31

--- App.tsx
+++ App.tsx
@@ -6,7 +6,21 @@ export default function App() {
   // string
   const [name,setName] = useState<string>('Hungdz');
 
- 
+ const [todoList,setTodoList] = useState([
+  {id:1,title: "Learn React Native 1"},
+  {id:2,title: "Learn React Native 2"},
+  {id:3,title: "Learn React Native 3"},
+  {id:4,title: "Learn React Native 4"},
+  {id:5,title: "Learn React Native 5"},
+  {id:6,title: "Learn React Native 6"},
+  {id:6,title: "Learn React Native 6"},
+  {id:6,title: "Learn React Native 6"},
+  {id:6,title: "Learn React Native 6"},
+  {id:6,title: "Learn React Native 6"},
+  {id:6,title: "Learn React Native 6"},
+  {id:6,title: "Learn React Native 6"},
+  {id:6,title: "Learn React Native 6"},
+ ])
 
   return (
     <View style={styles.container}>
@@ -26,7 +40,20 @@ export default function App() {
 
           <Text>{name}</Text>
 
-          <Button title='Add new'/>
+          <Button 
+          title='Add new' 
+          onPress={() => alert(23213)}
+          />
+
+          <View style={{marginTop:20}}>
+            {todoList.map(todo => {
+              return (
+                <Text 
+                style={styles.todo}
+                >{todo.title}</Text>
+              )
+            })}
+          </View>
       </View>
 
     </View>
@@ -37,6 +64,13 @@ export default function App() {
 // css in js 
 
 const styles = StyleSheet.create({
+
+  todo:{
+    fontSize:30,
+    backgroundColor:'pink',
+    marginBottom:10,
+    padding:10
+  },
   text: {
     fontSize:30,
     color:"red"
